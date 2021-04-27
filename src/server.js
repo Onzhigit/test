56c6561a70df335cdd96b321901a4342f83a3dcb
Yerkebulan <OnzhigitovY@gmail.com>
2021-04-27 15:55:51 +0600

    third commit

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8080;
 
 // здесь у нас происходит импорт пакетов и определяется порт нашего сервера
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.png')); 
+app.use(favicon(__dirname + '/build/favicon.ico')); 
 
 //здесь наше приложение отдаёт статику
 app.use(express.static(__dirname));
@@ -17,7 +17,7 @@ app.get('/ping', function (req, res) {
 });
 
 //обслуживание html
-app.get('/*', function (req, res) {
+app.get('/', function (req, res) {
 res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port);
